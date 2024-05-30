c427b004613bc52f1060286629711285e811c7f0
vikasb <vikas.bhargav@data-axle.com>
2024-05-30 19:07:22 +0530

    Added login, register and logout views

---
+++ social_media/friends/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        validated_data["username"] = validated_data["email"]
+        user = User.objects.create_user(**validated_data)
+        return user
